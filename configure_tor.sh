203fc4e288ce17ff923fe49d35be9629694f2cea
Terrtia <or1994@hotmail.fr>
2021-03-05 17:47:03 +0100

    fix: [tor config]

--- configure_tor.sh
+++ configure_tor.sh
@@ -3,9 +3,11 @@
 # halt on errors
 set -e
 
-sudo sed -i 's/#SOCKSPolicy accept 192.168.0.0\/16/SOCKSPolicy accept 172.17.0.0\/16/1' /etc/tor/torrc
+# sudo sh -c 'echo "deb https://deb.torproject.org/torproject.org $(lsb_release -sc) main" >> /etc/apt/sources.list.d/tor-project.list'
+
+sudo sed -i 's/#SocksPolicy accept 192.168.0.0\/16/SocksPolicy accept 172.17.0.0\/16/1' /etc/tor/torrc
 wait
-sudo sed -i 's/#SOCKSPort 192.168.0.1:9100/SOCKSPort 0.0.0.0:9050/1' /etc/tor/torrc
+sudo sed -i 's/#SocksPort 192.168.0.1:9100/SocksPort 0.0.0.0:9050/1' /etc/tor/torrc
 wait
 
 # restart tor proxy
